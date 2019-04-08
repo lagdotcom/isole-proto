ed55d51bf09a0c513498890dc5d2202fc73bd1c7
Paul Davies <lagdotcom@gmail.com>
2019-04-08 09:06:37 +0100

    added simple toggles for hitbox/fps display

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Game from './Game';
 
-var G;
+var controls, G;
+
+function addCheckbox(text, checked, onchange) {
+	var label = document.createElement('label');
+	label.innerText = text;
+	var input = document.createElement('input');
+	input.type = 'checkbox';
+	input.checked = checked;
+	input.onchange = onchange;
+
+	label.appendChild(input);
+	controls.appendChild(label);
+}
 
 window.addEventListener('load', () => {
 	G = new Game({
@@ -12,6 +24,16 @@ window.addEventListener('load', () => {
 		showHitboxes: true,
 	});
 	window.G = G;
+
+	controls = document.createElement('div');
+	addCheckbox('Show FPS', G.options.showFps, e => {
+		G.options.showFps = e.target.checked;
+	});
+	addCheckbox('Show hitboxes', G.options.showHitboxes, e => {
+		G.options.showHitboxes = e.target.checked;
+	});
+
+	document.body.appendChild(controls);
 
 	G.start();
 });
